35ec0280e07e2ce0707d3b083defc8ec809b12d9
yorwils <yorwi_torres_123@hotmail.com>
2022-02-21 15:50:55 -0500

    nuevo trabajo 1

---
+++ appratica 06/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appratica_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("ingrese las 3 notas seoaradas ");
+            double nota1 = double.Parse(Console.ReadLine());
+            double nota2 = double.Parse(Console.ReadLine());
+            double nota3 = double.Parse(Console.ReadLine());
+            double promedio = (nota1 + nota2 + nota3) / 3;
+
+
+            if (promedio >= 0)
+            {
+                if (promedio <= 5)
+                {
+                    Console.WriteLine("muy malo");
+                }
+                else if (promedio >= 6)
+                {
+                    if (promedio <= 10)
+                    {
+                        Console.WriteLine("malo");
+                    }
+                }
+                else if (promedio >= 11)
+                {
+                    if (promedio <= 12)
+                    {
+                        Console.WriteLine("regular");
+                    }
+                    else if (promedio >= 13)
+                    {
+                        if (promedio <= 16)
+                        {
+                            Console.WriteLine("bueno");
+                        }
+                    }
+                    else if (promedio >= 17)
+
+                    {
+                        if (promedio <= 20)
+                        {
+                            Console.WriteLine("muy bueno");
+                        }
+
+                    }
+                    
+
+                }
+            } Console.Read();
+        } 
+        
+    } 
+}
